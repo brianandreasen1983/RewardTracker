a67929d47488f316e1f3f7cd7776d31a9d974e8f
Brian Andreasen <bspinnfade@gmail.com>
2021-05-26 11:27:37 -0500

    Initial Commit

---
+++ RewardTrackerTests/RewardTrackerTests.swift
@@ -0,0 +1,33 @@
+//
+//  RewardTrackerTests.swift
+//  RewardTrackerTests
+//
+//  Created by Brian Andreasen on 5/26/21.
+//
+
+import XCTest
+@testable import RewardTracker
+
+class RewardTrackerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
